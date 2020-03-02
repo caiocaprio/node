2994b347cee76b5a91189bbc732eed134b196c70
Caio Caprio <caio.caprio@nextel.com.br>
2020-03-02 09:57:19 -0300

    add log

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ mongoose.connect(
 
 requireDir('./src/models');
 
+
 app.use(express.json())
 app.use('/api',require('./src/routes'))
 app.use(cors())
 
 app.listen(3001);
+console.log('http://localhost:3001')
